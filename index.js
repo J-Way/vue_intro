11be9a7dc9a408e87c7c5d534cc90726c24f7b55
Jordan Way <jordanway3@hotmail.ca>
2021-02-08 10:59:47 -0500

    basic intro complete

--- index.js
+++ index.js
@@ -21,8 +21,44 @@ var hover = new Vue({
 })
 
 var hover_if = new Vue({
-    el: "#hover_if",
+    el: "#vue_if",
     data: {
         seen: false
+    }
+})
+
+// not sure why this works with the spelling difference but it does
+// todo (.html) VS. todos (.js)
+var vue_for = new Vue({
+    el: "#vue_for",
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Learn Something else' }
+        ]
+    }
+})
+
+var vue_binding = new Vue({
+    el: "#vue_binding",
+    data: {
+        message: 'Change Me!?'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
     }
 })
